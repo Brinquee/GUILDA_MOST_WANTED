52f7113d989bafde1dc793dc432f01db6226a871
Brinque Scripts <gape.henry99@gmail.com>
2026-07-09 21:44:22 -0300

    Update Dwo84.lua

--- Dwo84.lua
+++ Dwo84.lua
@@ -47,8 +47,6 @@ local meusLinksDeMacros = {
     -- [SLOT 13]
     "https://raw.githubusercontent.com/Brinquee/GUILDA_MOST_WANTED/refs/heads/main/scripts/Guilda/Magiasempk.lua",  
   
-    -- [SLOT 14]
-    "https://raw.githubusercontent.com/Brinquee/GUILDA_MOST_WANTED/refs/heads/main/scripts/Guilda/FPS.lua",
 }
 
 -- Função interna que gerencia o download de um único arquivo por vez
