317de92696b3d5c0343b75d8e90e57100568a9a7
Brinque Scripts <gape.henry99@gmail.com>
2026-07-31 19:29:01 -0300

    Update and rename MWnoPE.lua to mwnopeBRQ.lua

--- scripts/Guilda/mwnopeBRQ.lua
+++ scripts/Guilda/mwnopeBRQ.lua
@@ -4,7 +4,7 @@ onPlayerPositionChange(function(newPos, oldPos)
     if oldPos then
         local tile = g_map.getTile(oldPos)
         if mw.isOn() and tile:isWalkable() then
-            useWith(7382, tile:getTopUseThing())
+            useWith(3188, tile:getTopUseThing())
         end
     end
 end)
