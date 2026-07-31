9e58b6c65a38602dfd82d2477590f799d402b832
Brinque Scripts <gape.henry99@gmail.com>
2026-07-31 19:00:32 -0300

    Add puxaraoredorBRQ.lua script

---
+++ scripts/Guilda/puxaraoredorBRQ.lua
@@ -0,0 +1,26 @@
+setDefaultTab("WAR")
+
+gpPushDelay = 200 -- safe value: 600ms
+
+macro(gpPushDelay, "PUXAR AO REDOR", "delete", function()
+        push(0, -1, 0)
+        push(0, 1, 0)
+        push(-1, -1, 0)
+        push(-1, 0, 0)
+        push(-1, 1, 0)
+        push(1, -1, 0)
+        push(1, 0, 0)
+        push(1, 1, 0)
+end)
+
+function push(x, y, z)
+    local position = player:getPosition()
+    position.x = position.x + x
+    position.y = position.y + y
+  
+    local tile = g_map.getTile(position)
+    local thing = tile:getTopThing()
+    if thing and thing:isItem() then
+      g_game.move(thing, player:getPosition(), thing:getCount())
+    end
+end
