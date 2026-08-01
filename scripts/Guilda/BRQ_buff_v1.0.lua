98a4b60311789476dfb3c0d06b512def9225cf34
Brinque Scripts <gape.henry99@gmail.com>
2026-08-01 01:04:15 -0300

    Change default tab from 'tools' to 'HP'

--- scripts/Guilda/BRQ_buff_v1.0.lua
+++ scripts/Guilda/BRQ_buff_v1.0.lua
@@ -6,7 +6,7 @@ local widgetRaizDoJogo = g_ui.getRootWidget()
 local idPainelBrinqueBuff = "janelaBrinqueBuffSetupVis"
 local idPainelEditBrinqueBuff = "janelaBrinqueBuffPopVis"
 
-setDefaultTab("tools") -- Aparece na aba Tools do seu menu lateral
+setDefaultTab("HP") -- Aparece na aba Tools do seu menu lateral
 
 -- =============================================================================
 -- [BLOCO 1] ARMAZENAMENTO DE CONFIGURAÇÕES (TOTALMENTE SEPARADO DO COMBO)
