bb688c3c799a993a4d76240b5ef9ba28e8b55192
Brinque Scripts <gape.henry99@gmail.com>
2026-08-05 23:50:52 -0300

    Update mwnopeBRQ.lua

--- scripts/Guilda/mwnopeBRQ.lua
+++ scripts/Guilda/mwnopeBRQ.lua
@@ -1,5 +1,8 @@
 setDefaultTab("guild")
 
+-- Força o VBot a atualizar o ID correto da MW e não ler o cache antigo
+local MWALL_ID = 3180 
+
 local mw = macro(100, "MW NO SEU PE", "F10", function() end)
 
 onPlayerPositionChange(function(newPos, oldPos)
@@ -7,7 +10,14 @@ onPlayerPositionChange(function(newPos, oldPos)
     
     local tile = g_map.getTile(oldPos)
     if mw.isOn() and tile and tile:isWalkable() then
-        -- Alterado de 3188 (Fire) para 3180 (Magic Wall)
-        useWith(3180, tile:getTopUseThing())
+        -- Usa o ID estrito definido acima (3180)
+        useWith(MWALL_ID, tile:getTopUseThing())
     end
+end)
+
+-- Caixa visual para você ver e testar o ID que o Bot está puxando
+UI.Label("ID da Mwall:")
+UI.TextEdit(tostring(MWALL_ID), function(widget, newText)
+    local newId = tonumber(newText)
+    if newId then MWALL_ID = newId end
 end)
