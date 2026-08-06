7a72b35a69103c2f7e23c31914e60fe1cc220ce9
Brinque Scripts <gape.henry99@gmail.com>
2026-08-05 23:48:00 -0300

    Refactor player position handling in mwnopeBRQ.lua
    Updated player position change logic to check tile existence before using the item.

--- scripts/Guilda/mwnopeBRQ.lua
+++ scripts/Guilda/mwnopeBRQ.lua
@@ -1,12 +1,13 @@
 setDefaultTab("guild")
 
-local mw = macro(100, "MW NO SEU PE", "F10",function() end)
+local mw = macro(100, "MW NO SEU PE", "F10", function() end)
+
 onPlayerPositionChange(function(newPos, oldPos)
-    if oldPos.z ~= posz() then return end
-    if oldPos then
-        local tile = g_map.getTile(oldPos)
-        if mw.isOn() and tile:isWalkable() then
-            useWith(3180, tile:getTopUseThing())
-        end
+    if not oldPos or oldPos.z ~= posz() then return end
+    
+    local tile = g_map.getTile(oldPos)
+    if mw.isOn() and tile and tile:isWalkable() then
+        -- Alterado de 3188 (Fire) para 3180 (Magic Wall)
+        useWith(3180, tile:getTopUseThing())
     end
 end)
