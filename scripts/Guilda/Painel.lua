55173e8fc6048bcf08c6f3333b57125f1acfeff5
Brinque Scripts <gape.henry99@gmail.com>
2026-08-09 01:48:57 -0300

    Fix image path for itachi_binque

--- scripts/Guilda/Painel.lua
+++ scripts/Guilda/Painel.lua
@@ -48,7 +48,7 @@ local configName = modules.game_bot.contentsPanel.config:getCurrentOption().text
 local pathBase = "/bot/" .. configName .. "/Imagens/"
 
 
-local imgFixa = pathBase .. "itachi_binque" 
+local imgFixa = pathBase .. "itachibinque" 
 
 -- Função para aplicar a imagem
 local function changeBotImage(path)
